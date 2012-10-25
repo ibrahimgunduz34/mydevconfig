bfa7dc12ed8123e8e12ec3d3bc9d174aa444455e
IbrahimGunduz <ibrahimgunduz@ibrahimgunduz-M56S-S3.(none)>
2012-10-25 08:44:10 +0300

    git instalation is added to preinstallation script

--- preinstall.sh
+++ preinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install vim curl apache2 php5 mysql-server mcrypt memcached php5-mysql php5-memcache php5-curl zend-framework phpmyadmin
+sudo apt-get install vim curl apache2 php5 mysql-server mcrypt memcached php5-mysql php5-memcache php5-curl zend-framework phpmyadmin git
