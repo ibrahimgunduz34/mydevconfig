07b1c13d61cae0ec9c52b30b98397fc69342fc6f
Ibrahim Gunduz <ibrahimgunduz@ibrahimgunduz-M56S-S3.(none)>
2012-10-26 00:09:35 +0300

    Updated phpunit installation script.

--- install-phpunit.sh
+++ install-phpunit.sh
@@ -1,15 +1,23 @@
-sudo apt-get remove phpunit
+sudo su
+apt-get remove phpunit
+pear config-set auto_discover 1
+pear channel-discover pear.phpunit.de
+pear channel-discover pear.symfony-project.com
+pear channel-discover components.ez.no
+pear update-channels
+pear upgrade-all
+pear install --alldeps phpunit/PHPUnit
+pear install --force --alldeps phpunit/PHPUnit
+pear install phpunit/DbUnit
+pear install phpunit/PHP_Invoker
+pear install phpunit/PHPUnit_Selenium
+pear install phpunit/PHPUnit_Story
+pear install phpunit/PHPUnit_SkeletonGenerator
+pear install phpunit/PHPUnit_TestListener_DBUS
+pear install phpunit/PHPUnit_TestListener_XHProf
+pear install phpunit/PHPUnit_TicketListener_Fogbugz
+pear install phpunit/PHPUnit_TicketListener_GitHub
+pear install phpunit/PHPUnit_TicketListener_GoogleCode
+pear install phpunit/PHPUnit_TicketListener_Trac
 
-sudo pear channel-discover pear.phpunit.de
 
-sudo pear channel-discover pear.symfony-project.com
-
-sudo pear channel-discover components.ez.no
-
-sudo pear update-channels
-
-sudo pear upgrade-all
-
-sudo pear install --alldeps phpunit/PHPUnit
-
-sudo pear install --force --alldeps phpunit/PHPUnit
